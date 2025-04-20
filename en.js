be150d538241c9b4f1f95a25629e1d3b0cd5fd59
GDuip <152042002+GDuip@users.noreply.github.com>
2025-04-20 00:13:11 -0500

    Create en.js

---
+++ en.js
@@ -0,0 +1,14 @@
+{
+    "loading": "Loading",
+    "score": "Score",
+    "wave": "Wave",
+    "health": "Health",
+    "enemies": "Enemies",
+    "paused": "Paused",
+    "resumeGame": "Resume Game",
+    "gameOver": "Game Over",
+    "finalScore": "Final Score",
+    "finalWave": "Reached Wave",
+    "restartButton": "Play Again",
+    "nextWaveIn": "Next wave in {seconds}s..."
+}
